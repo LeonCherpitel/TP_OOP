9e0af0f6e1d0b9e5fcceb705db25db1bf26c0bc6
LeonCherpitel <leoncherpitel@outlook.fr>
2026-09-15 17:57:04 +0200

    final

--- tp0/IA.py
+++ tp0/IA.py
@@ -12,4 +12,15 @@
 
 # Question 4
 
-#
+# READ ME fait 
+
+# Question 5 
+
+# Je dirai que oui, le vrai avantage est que la ligne de commande s'éxécute directement sans avoir besoin de copier coller
+
+# Question 6 
+
+# De manière le plus fiable, je trouve que l'utilisation de l'IA est la plus fiable quand il s'agit de relire/ modifier une 
+# fonction ou un programme sur lequel on bloque. Cela peut servir à apporter une solution mais ce qui est important est 
+# de comprendre comment réutiliser cette solution dans d'autres cas et ne pas seulement copier coller facilement. 
+# L'ia peut etre aussi efficace lors de debug( commme sur l'exo 3 avec l'erreur assert)
