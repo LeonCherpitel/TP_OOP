18803becb536ca5a79e01fd744a992dd0ccb7503
LeonCherpitel <leoncherpitel@outlook.fr>
2026-09-22 15:20:51 +0200

    exo5

--- tp2/village.py
+++ tp2/village.py
@@ -12,17 +12,29 @@ class Village:
 
     def afficher_habitants(self):
         for habitant in self.habitants:
-            print(f"- {habitant.nom}, {habitant.age} ans")
+            print(f"- {habitant.get_nom()}, {habitant.age} ans")
+
+    def get_habitants(self) -> list:
+        return self.habitants
+
 
 pytown = Village("PyTown")
 # composition
 pytown.ajouter_habitant_composition("Aldric", 25, "Rue A", {"vaches": 3})
 # agregation
-elise = Habitant("Élise", 28, "Rue B", {"poules":10})
+elise = Habitant("Élise", 28, "Rue B", {"poules": 10})
 pytown.ajouter_habitant_agregation(elise)
+
 autre_village = Village("VillageVoisin")
 autre_village.ajouter_habitant_agregation(elise)
 
-#ajouter_habitants_composition illustre une relation de composition entre Village et Habitant, car le village crée et possède l'habitant. 
-#Si le village est détruit, l'habitant créé par composition sera également détruit.
-#à l'inverse, ajouter_habitants_agregation illustre une relation d'agrégation, car le village utilise un habitant qui existe déja. 
+assert len(pytown.get_habitants()) == 2
+assert elise in autre_village.get_habitants()
+
+# ajouter_habitant_composition illustre une relation de composition entre Village
+# et Habitant, car le village crée et possède l'habitant. Si le village est
+# détruit, l'habitant créé par composition n'existe plus ailleurs et disparaît
+# avec lui.
+# À l'inverse, ajouter_habitant_agregation illustre une relation d'agrégation,
+# car le village utilise un habitant qui existe déjà indépendamment de lui, et
+# qui peut être partagé par plusieurs villages (comme élise ici).
