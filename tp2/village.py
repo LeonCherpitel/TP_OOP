40b2e1571dacceba3017e51e9e47131a1a223f42
LeonCherpitel <leoncherpitel@outlook.fr>
2026-09-22 14:55:35 +0200

    exo5

--- tp2/village.py
+++ tp2/village.py
@@ -4,13 +4,10 @@ class Village:
         self.habitants = []
 
     def ajouter_habitant_composition(self, nom: str, age: int, adresse: str, animaux: dict = None):
-
-        # composition : le village crée et possède l'habitant
         nouvel_habitant = Habitant(nom, age, adresse, animaux)
         self.habitants.append(nouvel_habitant)
 
     def ajouter_habitant_agregation(self, habitant: Habitant):
-        # agregation : le village utilise un habitant existant
         self.habitants.append(habitant)
 
     def afficher_habitants(self):
@@ -25,4 +22,7 @@ elise = Habitant("Élise", 28, "Rue B", {"poules":10})
 pytown.ajouter_habitant_agregation(elise)
 autre_village = Village("VillageVoisin")
 autre_village.ajouter_habitant_agregation(elise)
-# même habitant dans 2 villages
+
+#ajouter_habitants_composition illustre une relation de composition entre Village et Habitant, car le village crée et possède l'habitant. 
+#Si le village est détruit, l'habitant créé par composition sera également détruit.
+#à l'inverse, ajouter_habitants_agregation illustre une relation d'agrégation, car le village utilise un habitant qui existe déja. 
